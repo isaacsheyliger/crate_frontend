f07a1f81b5482ec68c0e427f1d0f3d70dcf2813c
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2023-12-23 11:48:28 -0500

    updated static file path

--- src/components/blog/home.js
+++ src/components/blog/home.js
@@ -2,21 +2,6 @@ import React from "react";
 import EmbedPlayer from "./embed";
 import Carousel from "./carousel";
 
-// likely not needed
-//
-// function useWindowSize() {
-//     const [size, setSize] = useState([0, 0]);
-//     useLayoutEffect(() => {
-//       function updateSize() {
-//         setSize([window.innerWidth, window.innerHeight]);
-//       }
-//       window.addEventListener('resize', updateSize);
-//       updateSize();
-//       return () => window.removeEventListener('resize', updateSize);
-//     }, []);
-//     return size;
-// }
-
 function Home(props) {
     return(
         <div id="home-body" className="body home-body">
