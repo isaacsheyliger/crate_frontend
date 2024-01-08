68d4be3c919f938f65a3e9396551fccf55e07205
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2024-01-08 11:54:31 -0500

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/nav.js'
 import Footer from './components/footer.js';
 import DigRoutes from './components/digger/routes.js';
 import BlogRoutes from './components/blog/routes.js';
-import { config } from './const/constants.js';
 //import './static/css';
-
-const BLOG_URL = config.blog_url
-let host = window.location.host;
-// let protocol = window.location.protocol;
-let parts = host.split(".");
-let subdomain = "";
-// If we get more than 3 parts, then we have a subdomain
 
 function App(props) {
   return (
     <BrowserRouter>
         <Nav />
         <Routes>
-          {/* <Route path='/login' element={<Login />}/> */}
-              <Route path='/blog' element={<BlogRoutes />}/>
-              {/* <Route path='/dig' element={<DigRoutes />}/> */}
-            {/* need route for django admin? */}
+              <Route path='#/blog' element={<BlogRoutes />}/>
+              {/* <Route path='#/dig' element={<DigRoutes />}/> */}
         </Routes>
         <Footer />
     </BrowserRouter>
