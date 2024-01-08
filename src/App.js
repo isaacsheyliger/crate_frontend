20e880dfb424097f6f9ef257a153791c21ea0e04
blkmoose <36351254+heyligerjon@users.noreply.github.com>
2024-01-08 12:53:09 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import DigRoutes from './components/digger/routes.js';
 import BlogRoutes from './components/blog/routes.js';
 //import './static/css';
 
+// Route to blog automatically
+window.location += '#/blog/'
+
 function App(props) {
   return (
-    <HashRouter>
+    <HashRouter basename=''>
         <Nav />
         <Routes>
-              <Route path='#/blog' element={<BlogRoutes />}/>
+              <Route path='/blog' element={<BlogRoutes />}/>
               {/* <Route path='#/dig' element={<DigRoutes />}/> */}
         </Routes>
         <Footer />
