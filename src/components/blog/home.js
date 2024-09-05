1efd2d87929f3187c00302ab0069c27eb1fdbef3
Jonathan Isaacs-Heyliger <heyligerjon@gmail.com>
2024-09-05 19:54:59 -0400

    create email list modal

--- src/components/blog/home.js
+++ src/components/blog/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Carousel from "./carousel";
+import Modal from "./modal";
 
 function Home(props) {
     return(
         <div id="home-body" className="body home-body">
             <section className="hero">
             <h1 className="header hero-header">&lt;recent.articles&gt;</h1>
+                <Modal />
                 <Carousel />
             </section>
         </div>
