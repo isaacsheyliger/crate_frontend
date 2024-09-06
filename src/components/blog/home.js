71945cdf505c9d288cbf869540a0384bdfb92096
Jonathan Isaacs-Heyliger <heyligerjon@gmail.com>
2024-09-06 14:24:36 -0400

    modal styling & functionality

--- src/components/blog/home.js
+++ src/components/blog/home.js
@@ -3,12 +3,19 @@ import Carousel from "./carousel";
 import Modal from "./modal";
 
 function Home(props) {
+    const [isActive, setIsActive] = React.useState(false);
+
+    const toggleModal = () => {
+        setIsActive(!isActive);
+    }
+
     return(
         <div id="home-body" className="body home-body">
             <section className="hero">
-            <h1 className="header hero-header">&lt;recent.articles&gt;</h1>
-                <Modal />
+                <h1 className="header hero-header">&lt;recent.articles&gt;</h1>
                 <Carousel />
+                <button onClick={toggleModal} className="button is-primary">Join our mailing list!</button>
+                <Modal closeModal={toggleModal} modalState={isActive} title="Join our mailing list!" />
             </section>
         </div>
     )
